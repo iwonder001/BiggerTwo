81ebce5fbca1270ae52d7e11842f0892430a54a8
Kristen Carethers <kcarethers@gmail.com>
2016-09-21 12:12:52 -0400

    Did code for the sumof a

--- src/Challange.java
+++ src/Challange.java
@@ -2,15 +2,29 @@
 public class Challange {
 
 	public static void main(String[] args) {
+		/*make a new method that Start with 2 int arrays, a and b, each length 2. 
+		Consider the sum of the values in each array. Return the array which has the largest sum. 
+		In event of a tie, return a.*/
 		
-		//Get sum of a
-		
-		//Get sum of b
-		
-		//Compare which is larger sum and return larger sum.  If a tie then return a.
+//		int[] test = { 1, 2 };
+//		int[] test2 = { 3, 4 };
 		
 		
 
 	}//psvm
+	public int [] biggerTwo(int[] a, int[] b) {
+		        //Get sum of a
+		int sumA = a[0];
+		int sumA2 = a[1];
+		int x = (sumA + sumA2);
+//		System.out.println(x);
+//		return x;
+		
+				//Get sum of b
+				
+		
+				//Compare which is larger sum and return larger sum.  If a tie then return a.
+		 
+	}//biggerTwo method close
 
 }//class
