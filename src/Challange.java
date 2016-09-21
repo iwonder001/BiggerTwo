047926e3a1f2bc200be854e4b0013f6b7763aa2c
Kristen Carethers <kcarethers@gmail.com>
2016-09-21 11:52:37 -0400

    Initial commit with to do list for challenge

---
+++ src/Challange.java
@@ -0,0 +1,16 @@
+
+public class Challange {
+
+	public static void main(String[] args) {
+		
+		//Get sum of a
+		
+		//Get sum of b
+		
+		//Compare which is larger sum and return larger sum.  If a tie then return a.
+		
+		
+
+	}//psvm
+
+}//class
